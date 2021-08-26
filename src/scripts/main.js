f6c371f022997ad7af560feea10ec49b46e9070a
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-26 14:14:10 -0500

    author dropdown is working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,23 @@
 //MODULE RESPONSIBILITIES: connect js to html using query selector on container id; define and call a render function (renders by calling PenPalSociety(), which is the function that returns the basic html for the site)
 
+import { fetchPenPals } from "./dataAccess.js"
 import { PenPalSociety } from "./PenPalSociety.js"
 
 
 const mainContainer = document.querySelector("#container")
 
+
+
 const render = () => {
-    mainContainer.innerHTML = PenPalSociety()
+    fetchPenPals()
+    //other fetch calls
+    .then(
+        () => {
+            mainContainer.innerHTML = PenPalSociety()
+        }
+    )
+
+    
 }
 
 render()
